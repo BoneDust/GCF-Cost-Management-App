bf0a6a43a2c01efdb5c84eae52334f87a22e93d4
Goodwill TSHEKELA <gtshekel@e4r23p5.wethinkcode.co.za>
2018-11-21 17:55:37 +0200

    user scribbles

--- app/node_api/user-handler/handler.js
+++ app/node_api/user-handler/handler.js
@@ -1,17 +1,65 @@
-const express = require('express')
-const serverless = require('serverless-http')
-const bodyParser = require('body-parser')
-const aws = require('aws-sdk')
-
+const express = require('express');
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const aws = require('aws-sdk');
 const app = express()
 
+const USERS_TABLE = process.env.USERS_TABLE;
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 app.use(bodyParser.json())//i dont know what this does
-app.use(bodyParser.urlencoded({ extended: true }))// this too i dont know
+//app.use(bodyParser.urlencoded({ extended: true }))// this too i dont know
 
 //endpoint function that returns all users
-app.get('/users', (req, res) => {
-   
-})
+app.get('/users', (req, res) => 
+	{
+		const 
+		res.status(200).json
+		({
+		
+			message: 'boom'
+		})
+
+
+
+	const params = {
+    	TableName: process.env.DYNAMODB_TABLE,
+    	Item: {
+      		id: uuid.v1(),
+      		text: data.text,
+      		checked: false,
+      		createdAt: timestamp,
+      		updatedAt: timestamp,
+    	},
+  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the todo item.',
+      });
+      return;
+    }
+	}
+
+	  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not get user' });
+    }
+    if (result.Item) {
+      const {userId, name} = result.Item;
+      res.json({ userId, name });
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
+  });
+);
 
 //endpoint function that returns a user by id
 app.get('/users/:id', (req, res) => {
