11071ca032b7cfa9f2dff783f5c9b3dc6024834c
mmayibo <mushagim@gmail.com>
2018-12-05 16:05:45 +0200

    service api

--- app/mobile/lib/service/api_service.dart
+++ app/mobile/lib/service/api_service.dart
@@ -3,13 +3,13 @@ import 'package:http/http.dart' as http;
 import 'package:cm_mobile/model/project.dart';
 import 'dart:convert';
 
-class ApiService{
+class ApiService {
   http.Client _client = new http.Client();
-   String _url = "";
+  String _url = "";
 
-   Future<List<Project>> getAll() async {
-     List<Project> resultList = [Project(id: 1)];
-     await Future.delayed(Duration(seconds: 2));
+  Future<List<Project>> getAll() async {
+    List<Project> resultList = [Project(id: 1)];
+    await Future.delayed(Duration(seconds: 2));
 
 //     await _client.get(Uri.parse(_url))
 //         .then((response) => response.body)
@@ -17,8 +17,8 @@ class ApiService{
 //         .then((json) => json["results"])
 //         .then((list) => list.forEach((item) => resultList.add(Project.fromJson(item))));
 
-     return resultList;
-   }
+    return resultList;
+  }
 
   Future<Project> get(String id) async {
     Project result = Project(id: 1);
