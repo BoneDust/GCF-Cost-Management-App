a684cf561763f1bf4991e87212e72fb31cabb7c9
Lonwabo Rarane <nrarane@student.42.fr>
2018-12-14 17:56:36 +0200

    user creation form
    show and hide password working properly

--- app/mobile/lib/screen/users/create_user_screen.dart
+++ app/mobile/lib/screen/users/create_user_screen.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
-class CreateUserScreen extends StatelessWidget {
+class CreateUserScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return CreateUserScreenState();
+  }
+}
+
+class CreateUserScreenState extends State<CreateUserScreen> {
+  bool _obscureText = true;
+
+  String _password;
+
+  void _toggle() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +27,86 @@ class CreateUserScreen extends StatelessWidget {
       body: ListView.builder(
         itemBuilder: (context, index) {
           return Form(
-            child: null,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10.0),
+                        labelText: 'Name'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10.0),
+                        labelText: 'Surname'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10.0),
+                        labelText: 'Username'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10.0),
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.remove_red_eye),
+                        onPressed: () {
+                          _toggle();
+                        },
+                      ),
+                    ),
+                    validator: (val) =>
+                        val.length < 6 ? 'Password too short.' : null,
+                    onSaved: (val) => _password = val,
+                    obscureText: _obscureText,
+                  ),
+                  // TextFormField(
+                  //   decoration: InputDecoration(
+                  //       contentPadding: EdgeInsets.all(10.0),
+                  //       labelText: 'Upload Picture'),
+                  // ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10.0),
+                        labelText: 'Contact Number'),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10.0),
+                        labelText: 'Select privilege'),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      RaisedButton(
+                        color: Colors.blue,
+                        onPressed: () {},
+                        child: Text(
+                          'Upload Image',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      RaisedButton(
+                        color: Colors.blue,
+                        onPressed: () {},
+                        child: Text(
+                          'Save',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           );
         },
         itemCount: 1,
